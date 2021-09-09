d704133bb8c0c2eaab7d037856a973e36c6394aa
heegeepark <phg980513@naver.com>
2021-09-09 14:55:06 +0900

    Feat: 분석 시작 버튼 액션함수 구현 완료 - 카메라VC 스토리보드 아이디 추가 - 분석 버튼 액션함수 구현

--- WhatDrink/WhatDrink/features/drinkDetector/DrinkDetectorViewController.swift
+++ WhatDrink/WhatDrink/features/drinkDetector/DrinkDetectorViewController.swift
@@ -24,7 +24,10 @@ class DrinkDetectorViewController: UIViewController {
     }
 
     @IBAction func detectDrinkButtonTapped(_ sender: Any) {
-        
+        let storyBoard = UIStoryboard(name: "Camera", bundle: nil)
+        let cameraVC = storyBoard.instantiateViewController(identifier: "CameraViewController") as! CameraViewController
+        cameraVC.modalPresentationStyle = .fullScreen
+        present(cameraVC, animated: false, completion: nil)
     }
     
 }
